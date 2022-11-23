d158b039e443d1114af1c6a0c62cfe659cd5dca6
kthdev46 <kouizibojeanclaude@gmail.com>
2022-11-23 11:21:23 +0100

    ajout de mon site en ligne

---
+++ js/main.js
@@ -0,0 +1,20 @@
+texte = document.getElementById("texte");
+navbarre = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+  navbarre.classList.toggle("sticky", window.scrollY > 10);
+});
+
+new Typewriter(texte, {})
+  .typeString(
+    'Le grand futur dans le <span style="color: #e95704; font-style: italic">NUMERIQUE</span> pour construire l\'Univer'
+  )
+  .pauseFor(300)
+  .deleteChars(8)
+  .typeString("le Monde")
+  .pause(1000)
+  .deleteChars(8)
+
+  .typeString("les Entreprises.")
+
+  .start();
